c90312b9b2716d6742725137c99f0be89fd2c623
medalorian <129704531+medalorian@users.noreply.github.com>
2023-04-02 20:40:58 -0500

    Update main.ts

--- js/main.ts
+++ js/main.ts
@@ -1,1 +1,16 @@
+async function fetchCSV(url: string): Promise<string[][]> {
+  const response = await fetch(url);
+  const data = await response.text();
+  const rows = data.split("\n");
+  return rows.map(row => row.split(","));
+}
 
+async function loadContent() {
+  const content = await fetchCSV("data/content.csv");
+  const contentContainer = document.getElementById("content");
+  if (contentContainer) {
+    contentContainer.innerHTML = content.map(row => `<p>${row.join(", ")}</p>`).join("");
+  }
+}
+
+loadContent();
