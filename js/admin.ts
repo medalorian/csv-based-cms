20ef6f31c339a4a24d156bf0f4c2622e34206d4b
medalorian <129704531+medalorian@users.noreply.github.com>
2023-04-02 22:44:55 -0500

    Update admin.ts

--- js/admin.ts
+++ js/admin.ts
@@ -1,11 +1,16 @@
-document.getElementById("add-content-form")?.addEventListener("submit", async (event) => {
+document.getElementById("add-content-form").addEventListener("submit", function (event) {
   event.preventDefault();
-  const newContent = (document.getElementById("new-content") as HTMLInputElement).value;
+  const newContent = document.getElementById("new-content").value;
   if (newContent) {
-    const content = await fetchCSV("data/content.csv");
-    content.push([newContent]);
-    // Save the updated CSV data
-    // Note: This step requires manual intervention to save the file, as JavaScript cannot directly write files to the repo.
-    alert("New content added! Please manually update the content.csv file in the 'data' folder with the new content.");
+    fetchCSV("data/content.csv")
+      .then(content => {
+        content.push([newContent]);
+        // Save the updated CSV data
+        // Note: This step requires manual intervention to save the file, as JavaScript cannot directly write files to the repo.
+        alert("New content added! Please manually update the content.csv file in the 'data' folder with the new content.");
+      })
+      .catch(error => {
+        console.error("Error loading content:", error);
+      });
   }
 });
