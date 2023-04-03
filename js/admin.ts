e026887ad4287a906794a876ffb0a4590f85749c
medalorian <129704531+medalorian@users.noreply.github.com>
2023-04-02 20:41:50 -0500

    Update admin.ts

--- js/admin.ts
+++ js/admin.ts
@@ -1,1 +1,11 @@
-
+document.getElementById("add-content-form")?.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const newContent = (document.getElementById("new-content") as HTMLInputElement).value;
+  if (newContent) {
+    const content = await fetchCSV("data/content.csv");
+    content.push([newContent]);
+    // Save the updated CSV data
+    // Note: This step requires manual intervention to save the file, as JavaScript cannot directly write files to the repo.
+    alert("New content added! Please manually update the content.csv file in the 'data' folder with the new content.");
+  }
+});
